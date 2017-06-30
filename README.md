e2fe597a4e4fb8d189c65cd7fc86392b92e2b396
Piotrek <piotrekk1@outlook.com>
2017-06-30 19:35:29 +0200

    Outliers

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ ud120-projects
 ==============
 
 Starter project code for students taking Udacity ud120
+
+Steps to start writing scripts:
+> activate udacity_ml
+
+> jupyter notebook
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -9,12 +9,30 @@ def outlierCleaner(predictions, ages, net_worths):
 
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
+        
+        Predictions are made using ages_train data
     """
     
     cleaned_data = []
+    errors = []
 
     ### your code goes here
-
+    for index, pred in enumerate(predictions):
+        square_of_error = (predictions[index] - ages[index]) ** 2
+        print square_of_error
+        errors.append(square_of_error)
+        
+    sorted(errors)
+    
+    print "Errors: ", errors
+    print "Limit: ", round(int(len(errors)*0.9))
+    error_limit = errors[int(round(len(errors)*0.9))]
+    print "error_limit: ", error_limit
+    
+    for index, pred in enumerate(predictions):
+        error = (predictions[index] - ages[index]) ** 2
+        if error < error_limit:
+            cleaned_data.append((ages[index], net_worths[index], error))
     
     return cleaned_data
 
